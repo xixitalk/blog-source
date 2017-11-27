0fc4b824337c2f08413b17d040660af32bbe5ebb
xixitalk <xixinxing@gmailcom>
2017-11-27 19:05:41 +0800

    blog source push Mon 27 Nov 19:05:41 HKT 2017

---
+++ push2source.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Add changes to git.
+git add -A content/post/*.md 
+git add -A static
+
+# Commit changes.
+msg="blog source push `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
