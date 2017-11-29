429df4f9b3fe3d911f6ec92d4976d3e38725ceb8
xixitalk <xixinxing@gmailcom>
2017-11-29 09:46:27 +0800

    blog source push Wed 29 Nov 09:46:27 HKT 2017

---
+++ all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "[ERROR]need comment to commit"
+  exit -1
+fi
+
+bash build.sh && bash deploy.sh $1 && bash push2source.sh
+
--- build.sh
+++ build.sh
@@ -3,5 +3,9 @@
 export http_proxy=http://192.168.1.104:8118
 export https_proxy=http://192.168.1.104:8118
 
+cd public
+rm -fr blog post css page tags twitter  categories
+cd ..
+
 hugo  --theme=Hugo-Octopress
 
